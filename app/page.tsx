d97699e2d080a95f0ba5385850a356225694c3ce
d03ariadna <arod_andaira@hotmail.com>
2023-07-12 19:26:58 -0600

    Little changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,7 @@ export default function HomePage() {
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
-        <a
-          href="https://vercel.fyi/roomGPT"
-          target="_blank"
-          rel="noreferrer"
-          className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
-        >
-          Clone and deploy your own with{" "}
-          <span className="text-blue-600">Vercel</span>
-        </a>
+        
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
           Generating dream rooms{" "}
           <span className="relative whitespace-nowrap text-blue-600">
@@ -36,21 +28,13 @@ export default function HomePage() {
         >
           Generate your dream room
         </Link>
-        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+        <div className="flex justify-between w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
-            <div className="flex sm:space-x-8 sm:flex-row flex-col">
+            <div className="flex sm:space-x-8 sm:flex-row flex-col justify-between">
               <div>
-                <h3 className="mb-1 font-medium text-lg">Original Room</h3>
-                <Image
-                  alt="Original photo of a room with roomGPT.io"
-                  src="/original-pic.jpg"
-                  className="w-full object-cover h-96 rounded-2xl"
-                  width={400}
-                  height={400}
-                />
+                <h1 className="mb-1 font-bold text-5xl text-left">About Us</h1>
               </div>
               <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
                 <Image
                   alt="Generated photo of a room with roomGPT.io"
                   width={400}
--- components/Header.tsx
+++ components/Header.tsx
@@ -16,15 +16,25 @@ export default function Header() {
           roomGPT.io
         </h1>
       </Link>
+      <div className="flex flex-col xs:flex-row justify-end items-center w-full mt-3 pb-7 sm:px-4 px-2">
+        <a
+        className="flex max-w-fit items-center justify-center space-x-2 rounded-full text-white px-5 py-2 mr-5 text-sm shadow-md hover:bg-blue-500 bg-gradient-to-r from-purple-500 to-blue-600 font-medium transition"
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p>Sign Up</p>
+      </a>
       <a
-        className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
+        className="flex max-w-fit items-center justify-center space-x-2 rounded-full text-white px-5 py-2 text-sm border border-white hover:border-transparent shadow-md hover:bg-gradient-to-r from-purple-500 to-blue-600 font-medium transition"
         href="https://github.com/Nutlope/roomGPT"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Github />
-        <p>Star on GitHub</p>
+        <p>Log In</p>
       </a>
+      </div>
+      
     </header>
   );
 }
