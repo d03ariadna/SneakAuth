503d8815bd691c782269e7188235f06db21dc236
d03ariadna <arod_andaira@hotmail.com>
2023-07-13 22:38:42 -0600

    FinalDesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import "../styles/globals.css";
 
-let title = "Dream Room Generator";
-let description = "Generate your dream room in seconds.";
+let title = "SneakAuth";
+let description = "Original in every feet";
 let ogimage = "https://roomgpt-demo.vercel.app/og-image.png";
-let sitename = "roomGPT.io";
+let sitename = "sneak-auth.vercel.app";
 
 export const metadata: Metadata = {
   title,
@@ -41,7 +41,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Londrina+Outline&family=Red+Hat+Display:wght@300;400;500;700;900&display=swap"
           rel="stylesheet"
         />
       </head>
--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function HomePage() {
       <div id="about"></div>
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4">
         <div className="flex justify-between w-full flex-col mt-20">
-          <div className="flex flex-col space-y-10 mt-5 mb-16">
+          <div className="flex flex-col space-y-10 mt-5 mb-6">
             <div className="flex sm:space-x-8 mb-20 sm:flex-row flex-col justify-between">
               <section id="AboutPage" className="flex">
-                <div className="w-1/3 mt-6">
+                <div className="w-1/3 mt-6 mr-10">
                   <h1 className="mb-8 font-bold text-5xl text-left">
                     About Us
                   </h1>
@@ -31,10 +31,10 @@ export default function HomePage() {
                 </div>
               </section>
             </div>
-            <div id="statics" className="pb-16"></div>
+            <div id="statics" className="pt-6 pb-16"></div>
             <section id="StaticsPage">
-              <h1 className="mb-1 font-bold text-5xl">Statics</h1>
-              <div className="flex justify-around">
+              <h1 className="mb-10 font-bold text-6xl">Statics</h1>
+              <div className="flex justify-center">
                 <div className="w-[514px] h-[314px] relative">
                   <div className="w-[500px] h-[300px] bg-[#262626] rounded-xl absolute right-0 bottom-0"></div>
                   <div className="bg-gradient-to-r from-bluemain to-[#650396] w-[500px] h-[300px] rounded-xl absolute top-0 left-0">
@@ -42,21 +42,21 @@ export default function HomePage() {
                   </div>
                 </div>
                 <div className="w-[514px] h-[314px] flex items-center">
-                  <h1 className="mb-1 font-bold text-6xl">
+                  <h1 className="mb-1 ml-10 font-outf text-6xl">
                     Sneaker Market Value
                   </h1>
                 </div>
               </div>
-              <div className="flex justify-around">
+              <div className="flex justify-center mt-24">
                 <div className="w-[514px] h-[314px] flex items-center">
-                  <h1 className="mb-1 font-bold text-6xl">
+                  <h1 className="mb-1 mr-10  font-outf text-6xl">
                     Counterfeit Shoe Market
                   </h1>
                 </div>
                 <div className="w-[514px] h-[314px] relative">
                   <div className="w-[500px] h-[300px] bg-[#262626] rounded-xl absolute right-0 bottom-0"></div>
-                  <div className="bg-gradient-to-r from-[#6d0c86] to-[#ab5200] w-[500px] h-[300px] rounded-xl absolute flex items-center top-0 left-0">
-                    <h1 className="mb-1 font-bold text-4xl">
+                  <div className="bg-gradient-to-r from-purplemain to-orange w-[500px] h-[300px] rounded-xl absolute flex items-center top-0 left-0">
+                    <h1 className="mb-1 mx-16 font-bold text-3xl">
                       In August 2018 were found 380,000 counterfeits sneakers
                       with a value of more than $73 millions
                     </h1>
@@ -67,15 +67,15 @@ export default function HomePage() {
           </div>
         </div>
 
-        <section id="project" className="mt-20 mb-24">
-          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl mt-10">
+        <section id="project" className="pt-32 mb-24">
+          <h1 className="mx-auto max-w-4xl font-display text-4xl font-bold tracking-normal text-gray-300 sm:text-6xl">
           What are we doing at{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             
             <span className="relative gradientText">SneakAuth</span>
           </span>{" "}?
           </h1>
-          <div className="flex justify-between w-full  flex-row sm:mt-10 pt-8">
+          <div className="flex justify-between w-full  flex-row sm:mt-10 pt-6 ">
             <div className="w-1/5 gradient shadow sm:p-1 md:p-1 mx-5 mb-24 rounded-3xl">
               <div className="w-full p-5 bg-black sm:p-6 md:p-8 rounded-3xl">
               <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
@@ -103,8 +103,8 @@ export default function HomePage() {
         </section>
 
         
-        <section id="roadmap" className="mt-20 mb-24">
-          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
+        <section id="roadmap" className="pt-36 mb-24">
+          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-6xl">
           {" "}
           <span className="relative whitespace-nowrap text-blue-600">
             
@@ -114,25 +114,25 @@ export default function HomePage() {
           <div className="flex justify-between w-full  flex-row sm:mt-8 p-4">
             <div className="w-1/4 max-w-sm shadow sm:p-6 md:p-8 dark:bg-gray-800 mx-5">
               <div className="mx-auto">
-                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q1</p>
+                <p className="font-outf text-6xl p-5 text-center text-white mb-5">Q1</p>
               </div>
               <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
             </div>
             <div className="w-1/4 max-w-sm bg-gradient-to-r shadow sm:p-6 md:p-8 mx-5">
               <div className="mx-auto">
-                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q2</p>
+                <p className="font-outf text-6xl p-5 text-center text-white mb-5">Q2</p>
               </div>
               <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
             </div>
             <div className="w-1/4 max-w-sm bg-gradient-to-r shadow sm:p-6 md:p-8 mx-5">
               <div className="mx-auto">
-                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q3</p>
+                <p className="font-outf text-6xl p-5 text-center text-white mb-5">Q3</p>
               </div>
               <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
             </div>
             <div className="w-1/4 max-w-sm bg-gradient-to-r shadow sm:p-6 md:p-8 mx-5">
               <div className="mx-auto">
-                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q4</p>
+                <p className="font-outf text-6xl p-5 text-center text-white mb-5">Q4</p>
               </div>
               <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
             </div>
@@ -141,8 +141,8 @@ export default function HomePage() {
 
 
 
-        <section id="team" className="mt-20 mb-36">
-          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
+        <section id="team" className="pt-36 mb-36">
+          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-6xl">
           Meet our{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             
@@ -150,26 +150,37 @@ export default function HomePage() {
           </span>{" "}
           </h1>
           <div className="flex justify-center w-full  flex-row sm:mt-10 pt-8">
-            <a href="https://www.linkedin.com/in/gibran-reyes/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
-              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            <a href="https://www.linkedin.com/in/gibran-reyes/" target={"_blank"} className="w-1/3 max-w-sm p-10 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <img src="https://media.licdn.com/dms/image/D4E03AQHoQBS-lHsWYg/profile-displayphoto-shrink_400_400/0/1666110944741?e=1694649600&v=beta&t=sgeGyDQan5m_fjcUDS26n3dImwo2w85LJ7GjgfgoJco" alt="Gibrán Reyes Picture" width={'250px'} className="mx-auto rounded-full drop-shadow-[0_35px_35px_rgba(20,0,255,0.25)]"/>
+              <p className="text-3xl font-outf text-white mt-5">Gibrán Reyes</p>
+              <p>FrontEnd Developer</p>
             </a>
-            
-            <a href="https://www.linkedin.com/in/ariadna-martinez30/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
-              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            <a href="https://www.linkedin.com/in/ariadna-martinez30/" target={"_blank"} className="w-1/3 max-w-sm p-10 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <img src="https://media.licdn.com/dms/image/D4E03AQEqFEcyuwPpkw/profile-displayphoto-shrink_400_400/0/1689307596509?e=1694649600&v=beta&t=hMeNT7gMcYq2PAKZC4xjx3BWxoCkKOpdoxIWIKIoCvo" alt="Ariadna Martínez Picture" width={'250px'} className="mx-auto rounded-full drop-shadow-[0_35px_35px_rgba(154,0,255,0.25)]"/>
+              <p className="text-3xl font-outf text-white mt-5">Ariadna Martínez</p>
+              <p>FrontEnd Developer</p>
             </a>
-            <a href="https://www.linkedin.com/in/rodrigo-se-mx/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
-              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            <a href="https://www.linkedin.com/in/rodrigo-se-mx/" target={"_blank"} className="w-1/3 max-w-sm p-10 sm:p-6 md:p-8 mx-5">
+              <img src="https://media.licdn.com/dms/image/D5603AQHlVtH1NJEUGQ/profile-displayphoto-shrink_400_400/0/1675462139075?e=1694649600&v=beta&t=qYa3NOWClnvkkuVt0trziGatckhRbI419R2wiB37qgc" alt="Rodrigo Espino Picture" width={'250px'} className="mx-auto rounded-full drop-shadow-[0_35px_35px_rgba(255,122,0,0.25)]"/>
+              <p className="text-3xl font-outf text-white mt-5">Rodrigo Espino</p>
+              <p>Blockchain Developer</p>
             </a>
           </div>
-          <div className="flex justify-center w-full  flex-row sm:mt-10 pt-8">
-            <a href="https://www.linkedin.com/in/abraham-orozco-m/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
-              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+          <div className="flex justify-center w-full  flex-row sm:mt-10 pt-4">
+            <a href="https://www.linkedin.com/in/abraham-orozco-m/" target={"_blank"} className="w-1/3 max-w-sm p-10 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <img src="https://media.licdn.com/dms/image/D4E03AQGxaby_QIhwDA/profile-displayphoto-shrink_400_400/0/1686293842019?e=1694649600&v=beta&t=yihwYxmbH_quUeZkFHQuDj0_njA1ecOqlGqg_JMOb1k" alt="Abraham Orozco Picture" width={'250px'} className="mx-auto rounded-full drop-shadow-[0_35px_35px_rgba(154,0,255,0.35)]"/>
+              <p className="text-3xl font-outf text-white mt-5">Abraham Orozco</p>
+              <p>Backend Developer</p>
             </a>
-            <a href="https://www.linkedin.com/in/jonathan-uriel-duran-carrasco-4a4845242/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
-              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            <a href="https://www.linkedin.com/in/jonathan-uriel-duran-carrasco-4a4845242/" target={"_blank"} className="w-1/3 max-w-sm p-10 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <img src="https://media.licdn.com/dms/image/D5603AQGQh85ZoFOwkg/profile-displayphoto-shrink_400_400/0/1685216779417?e=1694649600&v=beta&t=qtzh8dzcTcY8o7pQiZRuvJHimGXs-Y1lK9hpYJz_jUQ" alt="Uriel Duran Picture" width={'250px'} className="mx-auto rounded-full drop-shadow-[0_35px_35px_rgba(255,122,0,0.25)]"/>
+              <p className="text-3xl font-outf text-white mt-5">Uriel Durán</p>
+              <p>Blockchain Developer</p>
             </a>
-            <a href="https://www.linkedin.com/in/fernandoponce/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
-              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            <a href="https://www.linkedin.com/in/fernandoponce/" target={"_blank"} className="w-1/3 max-w-sm p-10 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <img src="fernando-ponce.jpg" alt="Fernando Ponce Picture" width={'250px'} className="mx-auto rounded-full drop-shadow-[0_35px_35px_rgba(20,0,255,0.25)]"/>
+              <p className="text-3xl font-outf text-white mt-5">Fernando Ponce</p>
+              <p>Backend Developer</p>
             </a>
           </div>
         </section>
@@ -181,7 +192,7 @@ export default function HomePage() {
           <form className="w-full mt-10 mb-20">
             <div className="flex items-center py-2">
               <input className="appearance-none bg-transparent w-full text-slate-200 mr-3 py-4 px-8 leading-tight focus:outline-none border-white rounded-2xl" type="text" placeholder="Write your email" aria-label="Full name" ></input>
-              <button className="flex-shrink-0 bg-white text-black shadow-md hover:bg-gradient-to-r from-purple-500 to-blue-600 font-bold hover:text-white py-4 px-8 rounded-2xl" type="button">
+              <button className="flex-shrink-0 bg-white text-black shadow-md hover:bg-gradient-to-r from-bluemain to-purplemain font-bold hover:text-white py-4 px-8 rounded-2xl" type="button">
                 Sign Up
               </button>
             </div>
