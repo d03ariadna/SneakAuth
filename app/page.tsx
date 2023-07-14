018fa66dc8079d621482576018bc554fa27a38ac
d03ariadna <arod_andaira@hotmail.com>
2023-07-13 18:15:23 -0600

    Project, roadmap, team, contact

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import SquigglyLines from "../components/SquigglyLines";
 
 export default function HomePage() {
   return (
-    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div className="flex max-w-7xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 bg-black">
         
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
           Generating dream rooms{" "}
@@ -46,6 +46,127 @@ export default function HomePage() {
             </div>
           </div>
         </div>
+
+        <section id="project" className="mt-20 mb-24">
+          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl mt-10">
+          What are we doing at{" "}
+          <span className="relative whitespace-nowrap text-blue-600">
+            
+            <span className="relative gradientText">SneakAuth</span>
+          </span>{" "}?
+          </h1>
+          <div className="flex justify-between w-full  flex-row sm:mt-10 pt-8">
+            <div className="w-1/5 gradient shadow sm:p-1 md:p-1 mx-5 mb-24 rounded-3xl">
+              <div className="w-full p-5 bg-black sm:p-6 md:p-8 rounded-3xl">
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </div>
+            </div>
+            <div className="w-1/5 gradient shadow sm:p-1 md:p-1 mx-5 mt-24 rounded-3xl">
+              <div className="w-full p-5 bg-black sm:p-6 md:p-8 rounded-3xl">
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </div>
+            </div>
+            <div className="w-1/5 gradient shadow sm:p-1 md:p-1 mx-5 mb-24 rounded-3xl">
+              <div className="w-full p-5 bg-black sm:p-6 md:p-8 rounded-3xl">
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </div>
+            </div>
+            <div className="w-1/5 gradient shadow sm:p-1 md:p-1 mx-5 mt-24 rounded-3xl">
+              <div className="w-full p-5 bg-black sm:p-6 md:p-8 rounded-3xl">
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </div>
+            </div>
+            {/* <div className="w-1/5 max-w-sm p-10 border-2 border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5 mt-24">
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </div> */}
+          </div>
+        </section>
+
+        
+        <section id="roadmap" className="mt-20 mb-24">
+          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
+          {" "}
+          <span className="relative whitespace-nowrap text-blue-600">
+            
+            <span className="relative gradientText">SneakAuth</span>
+          </span>{" "}Roadmap
+          </h1>
+          <div className="flex justify-between w-full  flex-row sm:mt-8 p-4">
+            <div className="w-1/4 max-w-sm shadow sm:p-6 md:p-8 dark:bg-gray-800 mx-5">
+              <div className="mx-auto">
+                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q1</p>
+              </div>
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div className="w-1/4 max-w-sm bg-gradient-to-r shadow sm:p-6 md:p-8 mx-5">
+              <div className="mx-auto">
+                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q2</p>
+              </div>
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div className="w-1/4 max-w-sm bg-gradient-to-r shadow sm:p-6 md:p-8 mx-5">
+              <div className="mx-auto">
+                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q3</p>
+              </div>
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+            <div className="w-1/4 max-w-sm bg-gradient-to-r shadow sm:p-6 md:p-8 mx-5">
+              <div className="mx-auto">
+                <p className="font-black text-5xl p-5 text-center text-white mb-5">Q4</p>
+              </div>
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur </p>
+            </div>
+          </div>
+        </section>
+
+
+
+        <section id="team" className="mt-20 mb-36">
+          <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
+          Meet our{" "}
+          <span className="relative whitespace-nowrap text-blue-600">
+            
+            <span className="relative gradientText">Team</span>
+          </span>{" "}
+          </h1>
+          <div className="flex justify-center w-full  flex-row sm:mt-10 pt-8">
+            <a href="https://www.linkedin.com/in/gibran-reyes/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </a>
+            
+            <a href="https://www.linkedin.com/in/ariadna-martinez30/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </a>
+            <a href="https://www.linkedin.com/in/rodrigo-se-mx/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </a>
+          </div>
+          <div className="flex justify-center w-full  flex-row sm:mt-10 pt-8">
+            <a href="https://www.linkedin.com/in/abraham-orozco-m/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </a>
+            <a href="https://www.linkedin.com/in/jonathan-uriel-duran-carrasco-4a4845242/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </a>
+            <a href="https://www.linkedin.com/in/fernandoponce/" target={"_blank"} className="w-1/4 max-w-sm p-10 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 mx-5">
+              <p className="text-sm text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta. Corrupti, doloribus quis vel officia temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam? temporibus totam necessitatibus amet cumque expedita illo excepturi eum sit fugiat mollitia, hic quaerat quisquam?</p>
+            </a>
+          </div>
+        </section>
+
+        <section id="signUp">
+          <h1 className="mx-auto max-w-4xl font-display text-4xl font-bold tracking-normal text-gray-300 sm:text-7xl ">
+          Want to know more?
+          </h1>
+          <form className="w-full mt-10 mb-20">
+            <div className="flex items-center py-2">
+              <input className="appearance-none bg-transparent w-full text-slate-200 mr-3 py-4 px-8 leading-tight focus:outline-none border-white rounded-2xl" type="text" placeholder="Write your email" aria-label="Full name" ></input>
+              <button className="flex-shrink-0 bg-white text-black shadow-md hover:bg-gradient-to-r from-purple-500 to-blue-600 font-bold hover:text-white py-4 px-8 rounded-2xl" type="button">
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </section>
       </main>
       <Footer />
     </div>
